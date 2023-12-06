c58742b17c37b081e67f65ff9e8dd7c1082277eb
erik-martins <erik.ozzetti.martins@gmail.com>
2023-12-05 23:26:46 -0300

    melhorias no styles

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -48,14 +48,12 @@
                             <ul class="block lg:flex">
                                 <li>
                                     <a href="#home" @click="navbarOpen = false"
-                                        :class="!navbarOpen && 'text-dark dark:text-dark-6': 'text-body-color dark:text-dark-6'"
                                         class="flex py-2 text-base font-medium text-body-color hover:text-dark dark:text-dark-6 dark:hover:text-black lg:ml-12 lg:inline-flex">
                                         Home
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="#about" @click="navbarOpen = false"
-                                        :class="!navbarOpen && 'text-dark dark:text-dark-6': 'text-body-color dark:text-dark-6'"
+                                    <a href="#about" @click="navbarOpen = false" 
                                         class="flex py-2 text-base font-medium text-body-color hover:text-dark dark:text-dark-6 dark:hover:text-black lg:ml-12 lg:inline-flex">
                                         Sobre
                                     </a>
