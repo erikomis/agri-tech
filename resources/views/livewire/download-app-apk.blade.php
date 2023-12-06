b093f41c6d871ae945a70a6b3b068f2d84ba55aa
erik-martins <erik.ozzetti.martins@gmail.com>
2023-12-05 23:16:54 -0300

    melhorias no styles

--- resources/views/livewire/download-app-apk.blade.php
+++ resources/views/livewire/download-app-apk.blade.php
@@ -12,6 +12,7 @@
             role="status">
             <span wire:loading
                 class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
-        </div </button>
+        </div>
+    </button>
 
 </div>
